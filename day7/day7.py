a903146181d2d3b3a81d757e560b4957efd5a769
Senek18 <kcisak26@gmail.com>
2024-12-07 09:36:40 +0100

    day7 **

---
+++ day7/day7.py
@@ -0,0 +1,40 @@
+from itertools import product
+
+with open("day6/input.txt", "r") as f:
+    data = f.read().splitlines()
+    final = {}
+    for row in data:
+        splits = row.split(": ")
+        final[int(splits[0])]= list(map(int, splits[1].split(" ")))
+
+count = 0
+operaators = ["+", "*"]
+
+for key, values in final.items():
+    lenght = len(values)-1
+    possible_combination = list(product(operaators, repeat=lenght))
+    
+    for combination in possible_combination:
+        if combination[0] == "+":
+            target = values[0] + values[1]
+        elif combination[0] == "*":
+            target = values[0] * values[1]
+
+        for i in range(2,lenght+1):
+            if combination[i-1] == "+":
+                target += values[i]
+            elif combination[i-1] == "*":
+                target *= values[i]
+            
+            if target > key:
+                break
+        
+        if target == key:
+            count += key
+            break
+
+print(count)
+                    
+
+            
+
